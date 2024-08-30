43eae0f183dbf27ff3c9ddf902ca04b447491a1e
ramin rowshan <raminrowshan153@gmail.com>
2024-08-30 19:15:32 +0330

    add : staging , adding and removing of checks

--- scripts/staging_update.sh
+++ scripts/staging_update.sh
@@ -2,4 +2,6 @@
 tmux send-keys -t staging-server 'C-c' Enter
 tmux send-keys -t staging-server 'git pull' Enter
 tmux send-keys -t staging-server 'alembic upgrade head' Enter
+tmux send-keys -t staging-server 'python3 -m app.scripts.delete_checks' Enter
+tmux send-keys -t staging-server 'python3 -m app.scripts.create_checks' Enter
 tmux send-keys -t staging-server 'PYTHONPATH=. python app/server.py --port 3002' Enter
