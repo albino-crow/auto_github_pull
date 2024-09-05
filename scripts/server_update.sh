22b6cc692fe3822e6135c97a66d52a596ed13261
ramin rowshan <raminrowshan153@gmail.com>
2024-09-05 18:49:36 +0330

    change to service

--- scripts/server_update.sh
+++ scripts/server_update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 tmux send-keys -t server 'C-c' Enter
+tmux send-keys -t staging-server 'sudo systemctl stop lumina_server_main.service' Enter
 tmux send-keys -t server 'git pull' Enter
 tmux send-keys -t server 'alembic upgrade head' Enter
 tmux send-keys -t server 'PYTHONPATH=. python app/server.py' Enter
+tmux send-keys -t staging-server 'sudo systemctl restart main_server_staging.service' Enter
+
--- scripts/staging_update.sh
+++ scripts/staging_update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 tmux send-keys -t staging-server 'C-c' Enter
+tmux send-keys -t staging-server 'sudo systemctl stop lumina_server_staging.service' Enter
 tmux send-keys -t staging-server 'git pull' Enter
 tmux send-keys -t staging-server 'alembic upgrade head' Enter
 tmux send-keys -t staging-server 'python3 -m app.scripts.delete_checks' Enter
 tmux send-keys -t staging-server 'python3 -m app.scripts.create_checks' Enter
-tmux send-keys -t staging-server 'PYTHONPATH=. python app/server.py --port 3002' Enter
+tmux send-keys -t staging-server 'sudo systemctl restart lumina_server_staging.service' Enter
