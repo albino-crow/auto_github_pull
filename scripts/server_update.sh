a614eb8f7601c15f9581834cc156e2aec954b7af
ramin rowshan <raminrowshan153@gmail.com>
2024-09-13 02:33:05 +0330

    fix auto update

--- scripts/server_update.sh
+++ scripts/server_update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 tmux send-keys -t server 'C-c' Enter
-tmux send-keys -t staging-server 'sudo systemctl stop lumina_server_main.service' Enter
+tmux send-keys -t server 'sudo systemctl stop lumina_server_main.service' Enter
 tmux send-keys -t server 'git pull' Enter
 tmux send-keys -t server 'alembic upgrade head' Enter
 tmux send-keys -t server 'PYTHONPATH=. python app/server.py' Enter
-tmux send-keys -t staging-server 'sudo systemctl restart main_server_staging.service' Enter
+tmux send-keys -t server 'sudo systemctl restart lumina_server_main.service' Enter
 
